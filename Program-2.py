a42a1fcef73c1dbabcde634586e1b533a02d9d79
MRaoShivani <114474204+MRaoShivani@users.noreply.github.com>
2022-09-27 13:52:19 +0530

    Add files via upload

---
+++ Program-2.py
@@ -0,0 +1,17 @@
+# Program-2
+#With a single integer as the input, generate the following until a = x [series of numbers as shown in the below examples
+
+
+a=int(input('a='))
+for i in range(1,a*2,2):
+    print(i,end=",")
+
+
+
+    
+
+
+
+    
+
+
